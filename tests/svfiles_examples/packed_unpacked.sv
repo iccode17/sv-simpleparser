b0f22ce9f9009fc8b60627627221ccc638719607
iccode17 <iccode17@gmail.com>
2025-06-09 18:32:29 +0200

    packed and unpacked data is not separated

---
+++ tests/svfiles_examples/packed_unpacked.sv
@@ -0,0 +1,15 @@
+// This file is public domain, it can be freely copied without restrictions.
+// SPDX-License-Identifier: CC0-1.0
+// Adder DUT
+`timescale 1ns/1ps
+
+module packed_unpacked (
+  input  [4:0] vec_i,
+  input  [4:0][3:0] matrix_i,
+  input  unpack_i [4:0],
+  input  [3:0] vec_unpack_i [4:0],
+  output out_o
+);
+
+
+endmodule
